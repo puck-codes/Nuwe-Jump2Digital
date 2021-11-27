bd6900b5432e5530fe2f0c1813c681cbe92e2d2b
BrayanSC <brayansc1119@gmail.com>
2021-11-27 23:58:40 +0100

    Entity Product

---
+++ src/main/java/tokyo/boblennon/nuwe/jump2digital/domain/product/Product.java
@@ -0,0 +1,32 @@
+package tokyo.boblennon.nuwe.jump2digital.domain.product;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "products")
+public @Getter @Setter @NoArgsConstructor class Product {
+
+    @Id
+    @NotNull
+    private UUID id;
+    
+    @NotEmpty
+    private String name;
+
+    @Positive
+    private Double price;
+
+    @NotNull
+    private String desc;
+    
+}
