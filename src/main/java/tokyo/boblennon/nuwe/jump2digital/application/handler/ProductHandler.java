33ccbfcc7924efa86d03e889c13073a51fdd7df4
BrayanSC <brayansc1119@gmail.com>
2021-11-28 00:02:31 +0100

    ProductHandler - Add method

---
+++ src/main/java/tokyo/boblennon/nuwe/jump2digital/application/handler/ProductHandler.java
@@ -0,0 +1,54 @@
+package tokyo.boblennon.nuwe.jump2digital.application.handler;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+import java.net.URI;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import tokyo.boblennon.nuwe.jump2digital.domain.product.Product;
+import tokyo.boblennon.nuwe.jump2digital.infrastructure.product.ProductRepositoryImp;
+
+@Component
+public class ProductHandler {
+
+    private final Validator validator;
+    private final ProductRepositoryImp productRepositoryImp;
+
+    @Autowired
+    public ProductHandler(final ProductRepositoryImp productRepositoryImp, final Validator validator) {
+        this.productRepositoryImp = productRepositoryImp;
+        this.validator = validator;
+    }
+
+    public Mono<ServerResponse> add(ServerRequest request){
+        Mono<Product> product = request.bodyToMono(Product.class);
+
+        return product.flatMap(p -> {
+            p.setId(UUID.randomUUID());
+            Errors errs = new BeanPropertyBindingResult(p, Product.class.getName());
+            this.validator.validate(p, errs);
+            if(errs.hasErrors()){
+                return Flux.fromIterable(errs.getFieldErrors())
+                    .map(err -> "Field " + err.getField() + " " + err.getDefaultMessage())
+                    .collectList()
+                    .flatMap(list -> ServerResponse.badRequest().bodyValue(list));
+            } else {
+                return this.productRepositoryImp.add(p)
+                    .flatMap(pData -> ServerResponse.created(URI
+                        .create("/product"))
+                        .contentType(APPLICATION_JSON)
+                        .bodyValue(pData));
+            }
+        });
+    }
+
+}
