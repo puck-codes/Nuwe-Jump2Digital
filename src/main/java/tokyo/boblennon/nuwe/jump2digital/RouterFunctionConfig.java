9862e485b371ee85f7a56091c591ea311128a248
BrayanSC <brayansc1119@gmail.com>
2021-11-28 00:03:18 +0100

    RouterFunctionConfig - POST_Add

---
+++ src/main/java/tokyo/boblennon/nuwe/jump2digital/RouterFunctionConfig.java
@@ -0,0 +1,26 @@
+package tokyo.boblennon.nuwe.jump2digital;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springdoc.core.annotations.RouterOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import tokyo.boblennon.nuwe.jump2digital.application.handler.ProductHandler;
+import tokyo.boblennon.nuwe.jump2digital.infrastructure.product.ProductRepositoryImp;
+
+@Configuration
+public class RouterFunctionConfig {
+
+    @Bean
+    @RouterOperation(beanClass = ProductRepositoryImp.class, beanMethod = "add")
+    public RouterFunction<ServerResponse> routes(ProductHandler productHandler) {
+        RouterFunction<ServerResponse> routes = route(
+                POST("/product"), productHandler::add);
+
+        return routes;
+    }
+}
