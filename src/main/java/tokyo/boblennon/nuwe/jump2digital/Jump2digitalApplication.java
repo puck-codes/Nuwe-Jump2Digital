a6d6e06af252c463267bfed742831e800e2eb889
BrayanSC <brayansc1119@gmail.com>
2021-11-28 00:06:44 +0100

    OpenAPI Swagger

--- src/main/java/tokyo/boblennon/nuwe/jump2digital/Jump2digitalApplication.java
+++ src/main/java/tokyo/boblennon/nuwe/jump2digital/Jump2digitalApplication.java
@@ -3,11 +3,17 @@ package tokyo.boblennon.nuwe.jump2digital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Nuwel JUMP2DIGITAL", version = "1.0", description = "Documentation APIs v1.0"))
+
 public class Jump2digitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jump2digitalApplication.class, args);
 	}
+
 
 }
