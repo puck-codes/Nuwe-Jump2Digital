8d088f4f9229d6cdda878b9506920c3ec0a859d3
BrayanSC <brayansc1119@gmail.com>
2021-11-27 19:48:23 +0100

    initial commit

---
+++ src/main/java/tokyo/boblennon/nuwe/jump2digital/Jump2digitalApplication.java
@@ -0,0 +1,13 @@
+package tokyo.boblennon.nuwe.jump2digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jump2digitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jump2digitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tokyo/boblennon/nuwe/jump2digital/Jump2digitalApplicationTests.java
@@ -0,0 +1,13 @@
+package tokyo.boblennon.nuwe.jump2digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jump2digitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
