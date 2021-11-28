da9d36cb69a4e00798ea542101779b1203cf529e
BrayanSC <brayansc1119@gmail.com>
2021-11-28 23:13:11 +0100

    ProductProjection entity, repository and implementation

--- src/main/java/tokyo/boblennon/nuwe/jump2digital/infrastructure/product/ProductMongoRepository.java
+++ src/main/java/tokyo/boblennon/nuwe/jump2digital/infrastructure/product/ProductMongoRepository.java
@@ -2,12 +2,18 @@ package tokyo.boblennon.nuwe.jump2digital.infrastructure.product;
 
 import java.util.UUID;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+import reactor.core.publisher.Flux;
 import tokyo.boblennon.nuwe.jump2digital.domain.product.Product;
+import tokyo.boblennon.nuwe.jump2digital.domain.product.ProductProjection;
 
 @Repository
 public interface ProductMongoRepository extends ReactiveMongoRepository<Product, UUID> {
+
+    @Aggregation(" { $group: { _id: $desc, amount: { $sum: 1 } } }, { $sort: { amount: 1 } }, { $project: { _id: 0, productType: $_id, amount: 1, } }")
+    Flux<ProductProjection> findProductsByProductType();
     
 }
